739f87d85c8e52924262542bea0a818d9ddebe9a
Pakkapon Phongthawee <me@pureapp.in.th>
2018-08-02 14:20:56 +0700

    update: jacobi source

--- inpaint_port/inpaint_spilibergman_simple_jacobi_GPU/uSolver.m
+++ inpaint_port/inpaint_spilibergman_simple_jacobi_GPU/uSolver.m
@@ -1,8 +1,8 @@
 % solve u subproblem using neuman bondary condition
-% this code using clamping tecnique to implement neuman bondary condition
-function u = uSolver(u,w,original_image,b,lambda,theta,max_gauss_seidel)
+% this code using jacobi technique
+function u = uSolver(u,w,original_image,b,lambda,theta,max_jacobi_loop)
     divergence = Divergence(w-b);    
-    for k = 1:max_gauss_seidel
+    for k = 1:max_jacobi_loop
         lapacian = imfilter(u,[0,1,0;1,0,1;0,1,0],'replicate');
         u = (1 ./ (lambda + 4*theta)) .* (lambda .*original_image - theta .* divergence + theta .* lapacian);  % remove h^2 for speed improve   
     end
